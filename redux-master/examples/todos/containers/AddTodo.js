a1e00a795c2ac8301e50f1e209741a1114041cd6
yjhcfzx <andyzheng84@outlook.com>
2017-01-17 20:55:00 +0800

    ignore nodemodules use bootstrap

--- redux-master/examples/todos/containers/AddTodo.js
+++ redux-master/examples/todos/containers/AddTodo.js
@@ -5,34 +5,38 @@ import { setSearchFilter } from '../actions'
 let AddTodo = ({ dispatch }) => {
   let input
   let search
- 
+
   return (
     <div>
-      <form onSubmit={e => {
+      <form  onSubmit={e => {
         e.preventDefault()
         if (!input.value.trim()) {
           return
         }
         dispatch(addTodo(input.value))
         input.value = ''
-      }}>
-      <div>
-        <input ref={node => {
-          input = node
-        }} />
-      
-        <button type="submit">
-          Add Todo
+      } }>
+        <div className='row'>
+          <div className="form-group col-sm-10">
+            <input className="form-control" ref={node => {
+              input = node
+            } } />
+          </div>
+          <button className="btn btn-primary col-sm-2" type="submit">
+            Add Todo
+      </button>
+        </div>
+        <div className='row'>
+          <div className="form-group col-sm-10">
+            <input className="form-control" ref={node => {
+              search = node
+            } } />
+          </div>
+          <button className="btn btn-primary col-sm-2" onClick={() => { dispatch(setSearchFilter(search.value)) } } type="button">
+            Search
         </button>
         </div>
-        <div>
-         <input ref={node => {
-          search = node
-        }} />
-       <button onClick={ ()=>{dispatch(setSearchFilter(search.value))} } type="button">
-         Search
-        </button>
-        </div>
+
       </form>
     </div>
   )
